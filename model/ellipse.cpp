e02dfd8b9a2e2671094a124c3e891bd7e7eddfef
flo1023 <florian_knop@hotmail.com>
2015-03-31 18:34:38 +0200

    Fixed and refactor ellipse intersections

--- model/ellipse.cpp
+++ model/ellipse.cpp
@@ -29,93 +29,50 @@ int Ellipse::intersects(const Line & l, std::vector<Point>& points)
     double lcm = umath::dlcm(xR*xR, yR*yR);
     double lcmx = lcm / (yR*yR);
     double lcmy = lcm / (xR*xR);
-
-    double x2, y2;
     double x = l.getPoint().getX();
     double y = l.getPoint().getY();
 
-    if (std::abs(std::fmod(l.getAngle(), M_PI)) == (M_PI_2))
-    {
+    bool verticalLine = (std::abs(std::fmod(l.getAngle(), M_PI)) == (M_PI_2));
 
+    if (verticalLine)
+    {
+        // Voir annexe rapport de projet
+        a = (lcmx);
+        b = (-2* lcmx * y1);
         c = (lcmy * x*x);
         c += (lcmy * x1*x1);
         c += (lcmx * y1*y1);
         c -= (2 * lcmy * x*x1);
         c -= (lcm);
-
-        a = (lcmx);
-        b = (-2* lcmx * y1);
-        rho = umath::rho(a, b ,c);
-
-        if (rho == 0)
-        {
-            y = -b / (2*a);
-            points.push_back(Point(x, y));
-        }
-        else if (rho > 0)
-        {
-            y = (-b + std::sqrt(rho)) / (2*a);
-            points.push_back(Point(x, y));
-            y = (-b - std::sqrt(rho)) / (2*a);
-            points.push_back(Point(x, y));
-
-        }
-
     }
     else
     {
-
-
-
-        // E == ((x - x1)² / a²) + ((y- y1)² / b²) = 1
-         // D == y = ax + b
-        // x = (y-b) / a
-       //  (((y-b)/a) - x1)² * lcmy +   lcmx * (y² + y1² - 2*y1*y) = lcm
-        // (((y-b)² / a² + x1² - 2(y-b)/a ) * lcmy + lcmx ... = lcm
-        // ((y² + b² - 2ay) / a + x1² - 2(y-b)/a )*lcmy + ...
-        // lcmy *( y² + b² - 2ay + ax1² - 2y + 2b) + (a*lcmx *(y² + y1² - 2*y1*y = lcm * a
-
-        // lcmy*y² + lcmy*b² - 2*lcmy*a*y + lcmy*a*x1² - lcmy*2*y + 2*lcmy*b
-          //            + a*lcmx*y² + a* lcmx *y1² - 2*a*lcmx*y1*y - lcm * a = 0
-
-
+        // Voir annexe rapport de projet
         slope = Geometry::getSlope(l.getAngle()); // pente -> a dans y = ax + b
         d = y - (slope * x); // d -> b dans y = ax + b
 
-        a = lcmy + (slope * lcmx);
+        a = lcmy + (slope * slope * lcmx);
+        b = -(2 * lcmy * d) - (2 * slope * lcmy * x1) - (2 * slope * slope * lcmx * y1);
+        c = lcmy * d * d;
+        c += (lcmy * slope * slope * x1 * x1);
+        c += (2 * slope * x1 * d);
+        c += (lcmx * slope * slope * y1 * y1);
+        c -= (lcm * slope * slope);
+    }
 
-        b = -(2 * lcmy * slope) - (2* lcmy) - (2 * slope * lcmx * y1);
-        std::cout << b << std::endl;
-        c = (lcmy * d * d);
-        c += (lcmy * slope * x1 * x1);
-        c += (2 * lcmy * d);
-        c += (slope * lcmx * y1 * y1);
-        c -= (lcm * slope);
-        std::cout << c;
-//        c = -lcm;
-//        c += lcmx * std::pow((d - y1), 2);
-//        c += lcmy * x1 * x1;
-//        a = lcmy + (lcmx * slope * slope);
-//        b = -(lcmy * 2 * x1) + (lcmx * 2 * slope * (d-y1));
+    rho = umath::rho(a, b, c);
 
-        rho = umath::rho(a, b, c);
-        std::cout << rho;
-        if (rho == 0)
+    if (rho >= 0)
+    {
+        y = (-b + std::sqrt(rho)) / (2*a);
+        x = verticalLine ? x : (y -d) / slope;
+        points.push_back(Point(x, y));
+
+        if (rho > 0)
         {
-            y = -b / (2*a);
-            x =  (y - d) / slope;
-            points.push_back(Point(x, y));
-        }
-        else if (rho > 0)
-        {
-            y = (-b + std::sqrt(rho)) / (2*a);
-            x = (y - d) / slope;
-            points.push_back(Point(x,y));
-
             y = (-b - std::sqrt(rho)) / (2*a);
-            x = (y - d) / slope;
+            x = verticalLine ? x : (y -d) / slope;
             points.push_back(Point(x,y));
-
         }
     }
 
