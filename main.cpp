cc0e9320222c2372c41c4343d2eca41b339fcb53
flo1023 <florian_knop@hotmail.com>
2015-04-03 20:46:56 +0200

    Add example lambda in main

--- main.cpp
+++ main.cpp
@@ -15,12 +15,35 @@
 #ifndef RUNTEST
 int main(int argc, char **argv)
 {
-    QApplication app(argc, argv);
-    Level * level = MapReader::level("levels/level1.lvl");
-    MapView box(&level);
-    box.show();
-    return app.exec();
+    //QApplication app(argc, argv);
+    //Level * level = MapReader::level("levels/level1.lvl");
 
+
+//    MapView box(&level);
+//    box.show();
+//    return app.exec();
+
+
+    std::vector<Point> vectors
+    {
+        Point(4,4),
+        Point(5,5),
+        Point(2,1),
+        Point(1,1),
+        Point(3,3),
+        Point(9,9)
+    };
+
+    std::sort(vectors.begin(), vectors.end(),
+        [](const Point& p1, const Point& p2) -> bool
+    {
+        double da = p1.distance(Point(0,0));
+        double db = p2.distance(Point(0,0));
+        return da < db;
+    });
+
+    for (auto &i : vectors)
+        std::cout << i << std::endl;
 
 
     return 0;
