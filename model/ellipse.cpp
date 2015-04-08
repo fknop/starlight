3b8aacb374f809444f5d4a77adeed2807b7c8476
flo1023 <florian_knop@hotmail.com>
2015-04-08 14:22:32 +0200

    FIX ELLIPSE/LINE INTERSECTION

--- model/ellipse.cpp
+++ model/ellipse.cpp
@@ -34,6 +34,13 @@ int Ellipse::intersects(const Line& l, std::vector<Point>& points)
     double lcmx = lcm / (yR*yR);
     double lcmy = lcm / (xR*xR);
 
+    std::cout << "D : " << d << std::endl;
+    std::cout << "SLOPE : " << slope << std::endl;
+    std::cout << "LCM : " << lcm << std::endl;
+    std::cout << "LCMX : " << lcmx << std::endl;
+    std::cout << "LCMY : " << lcmy << std::endl;
+
+    bool horizontalLine = umath::double_equals(0, slope);
     bool verticalLine = umath::double_equals(std::abs(std::fmod(l.angle(), M_PI)), (M_PI_2));
 
     if (verticalLine)
@@ -43,36 +50,76 @@ int Ellipse::intersects(const Line& l, std::vector<Point>& points)
         c = (lcmy * x * x) + (lcmy * x1 * x1) +(lcmx * y1 * y1)
                 - (2 * lcmy * x * x1) - (lcm);
     }
+    else if (horizontalLine)
+    {
+        // y = k
+        // lcmy(x - x1)² + lcmx(k - y1)² = lcm
+        // lcmy(x² + x1² - 2*x*x1) + lcmx (k - y1)² = lcm
+        // lcmy*x² + lcmy * x1² - lcmy * 2 * x * x1 + lcmx(k-y1)² - lcm = 0
+        // a = lcmy
+        // b = -2lcmy*x1
+        // c = lcmy*x1² + lcmx(k-y1)² - lcm
+        a = lcmy;
+        b = -2 * lcmy * x1;
+        c = (lcmy * x1 * x1) + (lcmx * (std::pow((y - y1), 2))) - lcm;
+    }
     else
     {
         a = lcmy + (slope * slope * lcmx);
-        b = -(2 * lcmy * d) - (2 * slope * lcmy * x1)
+        b = -(2 * lcmy * d)
+                - (2 * slope * lcmy * x1)
                 - (2 * slope * slope * lcmx * y1);
-        c = (lcmy * d * d) + (lcmy * slope * slope * x1 * x1)
-                + (2 * slope * x1 * d)
+
+        c = (lcmy * d * d)
+                + (lcmy * slope * slope * x1 * x1)
+                + (lcmy * 2 * slope * x1 * d)
                 + (lcmx * slope * slope * y1 * y1)
                 - (lcm * slope * slope);
     }
 
     rho = umath::rho(a, b, c);
 
+    std::cout << "A : " << a << std::endl;
+    std::cout << "B : " << b << std::endl;
+    std::cout << "C : " << c << std::endl;
+    std::cout << "RHO : " << rho << std::endl;
     if (rho >= 0)
     {
-        y = (-b + std::sqrt(rho)) / (2*a);
-        x = verticalLine ? x : (y -d) / slope;
+        if (horizontalLine)
+        {
+            x = (-b + std::sqrt(rho)) / (2*a);
+
+        }
+        else
+        {
+            y = (-b + std::sqrt(rho)) / (2*a);
+            x = verticalLine ? x : (y - d) / slope;
+        }
+
         points.push_back(Point(x, y));
 
         if (rho > 0)
         {
-            y = (-b - std::sqrt(rho)) / (2*a);
-            x = verticalLine ? x : (y -d) / slope;
+            if (horizontalLine)
+            {
+                x = (-b - std::sqrt(rho)) / (2*a);
+            }
+            else
+            {
+                y = (-b - std::sqrt(rho)) / (2*a);
+                x = verticalLine ? x : (y - d) / slope;
+            }
+
             points.push_back(Point(x,y));
         }
     }
 
 
     for (auto &i : points)
-        std::cout << i << std::endl;
+    {
+        std::cout << "ELLIPSE SIZE : " << this->center();
+        std::cout << "ELLIPSE INTERSECTION : " << i << std::endl;
+    }
 
 
     return points.size();
