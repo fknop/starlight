b1dfe28625f92a2818fea2cd71ec91736319284b
flo1023 <florian_knop@hotmail.com>
2015-04-10 21:01:11 +0200

    Fix collisions

--- model/level.h
+++ model/level.h
@@ -309,7 +309,7 @@ private:
     void sort_intersections(const Line& line,
                             std::vector<Intersection> &intersections);
 
-    bool check_collisions(const LineSegment &segment);
+    bool check_collisions(const LineSegment &segment, Mirror *m);
 };
 
 /* Fonctions inlines */
