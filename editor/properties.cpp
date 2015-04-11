fc042747fd749d9abd984252e75cfe82e9af05ed
Astalaseven <gatien.bovyn@gmail.com>
2015-04-11 15:14:16 +0200

    Fix property editor

--- editor/properties.cpp
+++ editor/properties.cpp
@@ -67,6 +67,7 @@ void Properties::set_element_prop(ElementView * ev)
         case ElementView::TypeView::MIRRORVIEW:
         {
             std::cout << "IT'S A MIRROR" << std::endl;
+            groupBox->layout()->removeWidget(prop_interface_);
             MirrorView * mv = dynamic_cast<MirrorView *> (ev);
             prop_interface_ = new MirrorProp(mv->mirror(), groupBox);
 
