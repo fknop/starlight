102ca48131b9688468be931948c9b97e7a7d6e7f
flo1023 <florian_knop@hotmail.com>
2015-04-12 18:50:44 +0200

    Delete old Line

--- model/line.cpp
+++ model/line.cpp
@@ -4,61 +4,6 @@
 #include "geometry.h"
 #include "line.h"
 
-
-
-//Line::Line(const Point& origin, double angle)
-//{
-//   this->origin_ = origin;
-//   this->angle_ = angle;
-//}
-
-//Line::Line(const Point& p1, const Point& p2)
-//{
-//   this->origin_ = p1;
-//   this->angle_ = Geometry::slope_to_rad(p1, p2);
-//}
-
-//bool operator==(const Line& l1, const Line& l2)
-//{
-//    return l1.angle_ == l2.angle_ && l1.origin_ == l2.origin_;
-//}
-
-//double Line::get_x(double y) const
-//{
-//    if (umath::equals(std::abs(std::fmod(this->angle_, M_PI)), (M_PI_2)))
-//    {
-//        return origin_.x();
-//    }
-//    else if (umath::equals(std::abs(std::fmod(this->angle_, M_PI)), (0)))
-//    {
-//        return 1; // on décide de renvoyer 1 car x peut prendre n'importe quelle valeur
-//    }
-//    else
-//    {
-//        double slope = Geometry::rad_to_slope(angle_);
-//        double b     = origin_.y() - (slope * origin_.x());
-//        return ((y - b) / slope);
-//    }
-//}
-//double Line::get_y(double x) const
-//{
-//    if (umath::equals(std::abs(std::fmod(this->angle_, M_PI)), (0)))
-//    {
-//        return origin_.y();
-//    }
-//    else if (umath::equals(std::abs(std::fmod(this->angle_, M_PI)), (M_PI_2)))
-//    {
-//        return 1; // on décide de renvoyer 1 car y peut prendre n'importe quelle valeur.
-//    }
-//    else
-//    {
-//        double slope = Geometry::rad_to_slope(angle_);
-//        double b     = origin_.y() - (slope * origin_.x());
-//        return (slope * x) + b;
-//    }
-//}
-
-
 Line::Line(double a, double b, double c)
 {
     if (umath::equals(a, 0) && umath::equals(b, 0))
--- model/line.h
+++ model/line.h
@@ -6,112 +6,14 @@
 #include "point.h"
 #include "umath.h"
 
-///*
-// * y = (-y*b)x + -a/y
-// * a = - by - c / x
-// * ax + by + c = 0;
-// * pente = -a / b
-// * terme = -c / b
-// *
-// * y = -ax -c / b
-// * y = -a/b
-// * x = -c/b
-// * Verticale si b = 0
-// * |a/b| > infini
-// */
-
-///**
-// * Cette classe représente une droite.
-// * Elle possède un point d'origine et un
-// * angle. De cette manière on peut
-// * trouver n'importe quel point se trouvant
-// * sur la droite en ayant une distance.
-// */
-//class Line
-//{
-//    Point origin_;
-//    double angle_;
-
-//public:
-//    /**
-//     * Instancie une droite, avec un point donné et
-//     * un angle en radians correspondant à la pente.
-//     * @param origin un point de la droite.
-//     * @param angle l’angle de la pente.
-//     */
-//    Line(const Point& origin, double angle);
-
-//    /**
-//     * Instancie une droite avec deux points donnés.
-//     * @param p1 un premier point de la droite.
-//     * @param p2 un deuxième point de la droite.
-//     */
-//    Line(const Point& p1, const Point& p2);
-
-//    /**
-//     * Renvoie un point de la droite.
-//     * @return un point de la droite.
-//     */
-//    inline const Point& origin() const;
-
-//    /**
-//     * Modifie le point d'origine de la droite.
-//     * @param origin le nouveau point d'origine.
-//     */
-//    inline void set_origin(const Point& origin);
-
-//    /**
-//     * Retourne la pente de la droite.
-//     * @return la pente de la droite.
-//     */
-//    inline double angle() const;
-
-//    /**
-//     * Modifie l'angle de la droite.
-//     * @param le nouvel angle en radians de la droite.
-//     */
-//    inline void set_angle(double rad);
-
-//    double get_x(double y) const;
-//    double get_y(double x) const;
-
-//    /**
-//     * Redéfinition de l'opérateur d'égalité, renvoie vrai
-//     * si les deux droites ont le même angle et point
-//     * d'origine.
-//     * @param l1 la première droite.
-//     * @param l2 la deuxième droite.
-//     * @return vrai si les droites sont égales, faux sinon.
-//     */
-//    friend bool operator==(const Line& l1, const Line& l2);
-
-//private:
-//    static bool vertical_line_intersection(const Line& l1, const Line& l2,
-//                                              Point** intersection);
-//};
-
-///* Fonctions inlines */
-
-
-//const Point& Line::origin() const
-//{
-//    return this->origin_;
-//}
-
-//void Line::set_origin(const Point &origin)
-//{
-//    this->origin_ = origin;
-//}
-
-//double Line::angle() const
-//{
-//    return this->angle_;
-//}
-
-//void Line::set_angle(double angle)
-//{
-//    this->angle_ = angle;
-//}
+/**
+ * Cette classe représente une droite
+ * sous la forme ax + by + c = 0.
+ * Elle possède également un attribut
+ * alpha_ pour préciser l'angle réel de la droite.
+ * Si on veut travailler avec des angles négatifs
+ * par exemple.
+ */
 class Line
 {
     double a_;
@@ -122,9 +24,30 @@ class Line
 
     public:
 
+    /**
+     * Instancie une nouvelle droite avec les
+     * trois paramètres a, b et c donnés.
+     * @param a a
+     * @param b b
+     * @param c c
+     */
     Line(double a, double b, double c);
+
+    /**
+     * Instancie une nouvelle droite avec deux
+     * points donnés.
+     * @param a le premier point.
+     * @param b le deuxième point.
+     */
     Line(const Point& a, const Point& b);
-    Line(const Point& a, double alpha);
+
+    /**
+     * Instancie une nouvelle droite avec un
+     * point donné et un angle donné.
+     * @param p le point de la droite
+     * @param alpha l'angle de la droite
+     */
+    Line(const Point& p, double alpha);
 
     /**
      * Retourne a.
