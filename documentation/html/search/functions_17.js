992db61d69cc7dc78e72cf2d540b75953216f4fd
flo1023 <florian_knop@hotmail.com>
2015-04-24 10:14:05 +0200

    update doc

--- documentation/html/search/functions_17.js
+++ documentation/html/search/functions_17.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['_7emapview',['~MapView',['../de/d8c/classMapView.html#a6c3788b3c08e116287872347b3b0e9f6',1,'MapView']]],
   ['_7eobservable',['~Observable',['../d9/db4/classObservable.html#a058acb5a674eba2b96648c66a27440d8',1,'Observable']]]
 ];
